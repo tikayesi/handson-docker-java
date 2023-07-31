bb1777034eff1d4c2f8de5e8893f4dd8f12aa742
Tika Yesi Kristiani <yesikristianitika@gmail.com>
2023-07-31 16:02:13 +0700

    able to show hello world

--- src/main/java/com/example/simpletodo/controller/ToDoController.java
+++ src/main/java/com/example/simpletodo/controller/ToDoController.java
@@ -1,34 +1,14 @@
 package com.example.simpletodo.controller;
 
-import com.example.simpletodo.entity.ToDo;
-import com.example.simpletodo.service.ToDoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 public class ToDoController {
-    ToDoService toDoService;
-
-    @Autowired
-    public ToDoController(ToDoService toDoService) {
-        this.toDoService = toDoService;
-    }
-
-    @PostMapping("/todos")
-    public ToDo save(@RequestBody ToDo toDo){
-        return toDoService.createTodo(toDo);
-    }
 
     @GetMapping("/todos")
-    public List<ToDo> getList(){
-        return toDoService.getTodolist();
-    }
-
-    @GetMapping("/todos/{id}")
-    public ToDo getDetail(@PathVariable String id){
-        return toDoService.getDetail(id);
+    public String getDetail(){
+        return "Hello world!";
     }
 
 }
--- src/main/java/com/example/simpletodo/repository/ToDoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.simpletodo.repository;
-
-import com.example.simpletodo.entity.ToDo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ToDoRepository extends JpaRepository<ToDo, String> {
-}
